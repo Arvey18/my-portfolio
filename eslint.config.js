799dee6aba661b292d18ca3df835a064bea159e4
Arvey Jimenez <arve505@gmail.com>
2024-11-04 19:59:22 +0800

    update eslint config.

--- eslint.config.js
+++ eslint.config.js
@@ -6,41 +6,38 @@ import tseslint from '@typescript-eslint/eslint-plugin';
 import tsParser from '@typescript-eslint/parser';
 import prettier from 'eslint-plugin-prettier';
 
-export default {
-  ignores: ['dist'],
-  files: ['**/*.{js,ts,tsx}'],
-  languageOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-    globals: globals.browser,
-    parser: tsParser,
-  },
-  settings: {
-    react: {
-      version: 'detect',
+export default [
+  {
+    ignores: ['dist'],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      globals: globals.browser,
+      parser: tsParser,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      '@typescript-eslint': tseslint,
+      prettier,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
+      ...prettier.configs.recommended.rules, // Prettier rules directly
+      '@typescript-eslint/no-unused-vars': ['warn'],
+      'react/react-in-jsx-scope': 'off', // Not needed with React 17+
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'prettier/prettier': 'warn',
     },
   },
-  extends: [
-    js.configs.recommended,
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:prettier/recommended',
-  ],
-  plugins: {
-    react,
-    'react-hooks': reactHooks,
-    '@typescript-eslint': tseslint,
-    prettier,
-  },
-  rules: {
-    ...reactHooks.configs.recommended.rules,
-    '@typescript-eslint/no-unused-vars': ['warn'],
-    'react/react-in-jsx-scope': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-    'prettier/prettier': 'warn',
-  },
-};
+];
