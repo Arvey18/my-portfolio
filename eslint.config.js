c167f9a872691ae9889933ed3af7aaca9e074bde
Arvey Jimenez <arve505@gmail.com>
2024-11-04 19:59:42 +0800

    update eslint config.

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,7 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import tseslint from '@typescript-eslint/eslint-plugin';
 import tsParser from '@typescript-eslint/parser';
 import prettier from 'eslint-plugin-prettier';
+import reactRefresh from 'eslint-plugin-react-refresh'; // Import the react-refresh plugin
 
 export default [
   {
@@ -14,25 +15,24 @@ export default [
       ecmaVersion: 2020,
       sourceType: 'module',
       globals: globals.browser,
-      parser: tsParser,
-    },
-    settings: {
-      react: {
-        version: 'detect',
-      },
+      parser: tsParser, // Specify the TypeScript parser
     },
     plugins: {
       react,
       'react-hooks': reactHooks,
       '@typescript-eslint': tseslint,
       prettier,
+      'react-refresh': reactRefresh,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
+      ...js.configs.recommended.rules,
       ...tseslint.configs.recommended.rules,
-      ...prettier.configs.recommended.rules, // Prettier rules directly
-      '@typescript-eslint/no-unused-vars': ['warn'],
-      'react/react-in-jsx-scope': 'off', // Not needed with React 17+
+      ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
